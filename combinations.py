18808884118a014b8bae8c96c6a7af2715c3b757
philippevigneron <philippe@Philippes-MacBook-Pro.local>
2020-10-09 16:04:09 -0700

    adding performance measurements

--- combinations.py
+++ combinations.py
@@ -1,16 +1,25 @@
-steps = 10
-kinds = [1,3,5]
+from datetime import datetime
+iteration_nb = 17
+moves = [1,2]
+solutions = 0
+start = datetime.now()
 
 def take_a_step(step):
-  if (sum(step) >= steps): print('{0}'.format(step))
-  for j in kinds:
+  if (sum(step) >= iteration_nb): 
+    print(step)
+    global solutions
+    solutions+=1
+  for j in moves:
     newstep = []
     newstep.extend(step)
     newstep.append(j)
-    if (sum(newstep) <= steps): 
+    if (sum(newstep) <= iteration_nb): 
       take_a_step(newstep)
 
-for i in kinds:
+for i in moves:
   newstep = []
   newstep.append(i)
   take_a_step(newstep)
+end = datetime.now()
+print('Computation time: ',round((end-start).total_seconds(),3))
+print('# of solutions: ',solutions)
