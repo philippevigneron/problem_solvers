ecaf2af6752ea7e8425d34a3ed699968490b655c
philippevigneron <philippe@Philippes-MacBook-Pro.local>
2020-10-27 17:12:29 -0700

    adding method 3, most efficient

--- arraysmergesort.py
+++ arraysmergesort.py
@@ -25,7 +25,7 @@ def gosort1():
         ar1.insert(j,ar2[i])
         break
       if j == (len(ar1)-1): ar1.append(ar2[i])
-  print(ar1)
+  #print(ar1)
   end = datetime.now()
   print('Computation time: ',round((end-start).total_seconds(),3))
 
@@ -43,9 +43,33 @@ def gosort2():
         ar1.insert(j,ar2[i])
         break
     if j == (len(ar1)-1): ar1.append(ar2[i])
-  print(ar1)
+  #print(ar1)
   end = datetime.now()
   print('Computation time: ',round((end-start).total_seconds(),3))
 
+def gosort3():
+  print('Method 3')
+  ar1 = generate_random_array(size)
+  ar2 = generate_random_array(size)
+  if size <= 50: print(ar1,ar2)
+  start = datetime.now()
+  res = []
+  i = j = 0
+  for k in range(len(ar1) + len(ar2)):
+    if (i < len(ar1) and j < len(ar2)) and ar1[i] < ar2[j]:
+      res.append(ar1[i])
+      i += 1
+    else:
+      if j < len(ar2):
+        res.append(ar2[j])
+        j += 1
+      if i < len(ar1):
+        res.append(ar1[i])
+        i += 1
+  #print(res)
+  end = datetime.now()
+  print('Computation time: ', round((end - start).total_seconds(), 3))
+
 gosort1()
 gosort2()
+gosort3()
